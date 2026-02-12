a21cf5c2ae461db8bfeced20af46201a5e9a1b61
Ahmad Molhim <ahmadmolhim04@hotmail.com>
2026-02-11 20:25:53 -0500

    DONE

--- 1101_hw1/alu.v
+++ 1101_hw1/alu.v
@@ -49,78 +49,93 @@ module alu (
   // ---------------------------------------------------------------------------
   // ---- Write your conbinational block design here ---- //
 
-  always @(*) begin
+  always @(i_clk) begin
 
     o_valid_w = 1'b0;
     o_overflow_w = 1'b0;
+    o_data_w = 12'b0;
     mac_overflow_res_w = 1'b0;
+    sum_w = 13'b0;
+    diff_w = 13'b0;
+    mult_result_w = 24'b0;
+    rounded_w = 25'b0;
+    fixed_format_w = 24'b0;
+    mac_mult_result_w = 24'b0;
+    mac_rounded_w = 25'b0;
+    mac_fixed_format_w = 24'b0;
     accumulator_w = prev_accumulator_r;
+    mean_w = 13'b0;
+    abs_a_w = 12'b0;
+    abs_b_w = 12'b0;
+
 
     if (i_valid) begin
 
       o_valid_w = 1'b1;
+
       if (i_inst != 3'b011) begin
-	accumulator_w = 13'b0;
+        accumulator_w = 13'b0;
       end
 
 
-        case (i_inst)
-          3'b000: begin
-            sum_w = i_data_a + i_data_b;
-            o_data_w = sum_w[11:0];
-            o_overflow_w = (i_data_a[11] == i_data_b[11]) && (sum_w[11] != i_data_a[11]);
-          end  // ADD
+      case (i_inst)
+        3'b000: begin
+          sum_w = i_data_a + i_data_b;
+          o_data_w = sum_w[11:0];
+          o_overflow_w = (i_data_a[11] == i_data_b[11]) && (sum_w[11] != i_data_a[11]);
+        end  // ADD
 
-          3'b001: begin
-            diff_w = i_data_a - i_data_b;
-            o_data_w = diff_w[11:0];
-            o_overflow_w = (i_data_a[11] != i_data_b[11]) && (i_data_a[11] != diff_w[11]);
-          end  // SUBTRACT
+        3'b001: begin
+          diff_w = i_data_a - i_data_b;
+          o_data_w = diff_w[11:0];
+          o_overflow_w = (i_data_a[11] != i_data_b[11]) && (i_data_a[11] != diff_w[11]);
+        end  // SUBTRACT
 
-          3'b010: begin
-            mult_result_w = i_data_a * i_data_b;
-            rounded_w = mult_result_w + 25'd16;
-            fixed_format_w = {{5{rounded_w[23]}}, rounded_w[23:5]};  //sign extend and shift by 5
-            o_overflow_w = ({12{fixed_format_w[11]}} != fixed_format_w[23:12]);
-            o_data_w = fixed_format_w[11:0];
-          end  // MULTIPLY
+        3'b010: begin
+          mult_result_w = i_data_a * i_data_b;
+          rounded_w = mult_result_w + 25'd16;
+          fixed_format_w = {{5{rounded_w[23]}}, rounded_w[23:5]};  //sign extend and shift by 5
+          o_overflow_w = ({12{fixed_format_w[11]}} != fixed_format_w[23:12]);
+          o_data_w = fixed_format_w[11:0];
+        end  // MULTIPLY
 
-          3'b011: begin
-            mac_mult_result_w = i_data_a * i_data_b;
-            mac_rounded_w = mac_mult_result_w + 25'd16;
-            mac_fixed_format_w = {{5{mac_rounded_w[23]}}, mac_rounded_w[23:5]};
-            accumulator_w = prev_accumulator_r + mac_fixed_format_w;
-            o_data_w = accumulator_w[11:0];
-            mac_overflow_res_w = (({12{mac_fixed_format_w[11]}} != mac_fixed_format_w[23:12]) || ((prev_accumulator_r[12] == mac_fixed_format_w[11]) && (accumulator_w[11] != mac_fixed_format_w[11])));
-            o_overflow_w = prev_overflow_r || mac_overflow_res_w;
-          end  // MAC
+        3'b011: begin
+          mac_mult_result_w = i_data_a * i_data_b;
+          mac_rounded_w = mac_mult_result_w + 25'd16;
+          mac_fixed_format_w = {{5{mac_rounded_w[23]}}, mac_rounded_w[23:5]};
+          accumulator_w = prev_accumulator_r + {mac_fixed_format_w[11], mac_fixed_format_w[11:0]};
+          o_data_w = accumulator_w[11:0];
+          mac_overflow_res_w = (({12{mac_fixed_format_w[11]}} != mac_fixed_format_w[23:12]) || ((prev_accumulator_r[12] == mac_fixed_format_w[11]) && (accumulator_w[11] != mac_fixed_format_w[11])));
+          //({2{accumulator_w[11]}} != accumulator_w[12:11]);
+          o_overflow_w = prev_overflow_r || mac_overflow_res_w;
+        end  // MAC
 
-          3'b100: begin
-            o_data_w = ~(i_data_a ^ i_data_b);
-          end  // XNOR
+        3'b100: begin
+          o_data_w = ~(i_data_a ^ i_data_b);
+        end  // XNOR
 
-          3'b101: begin
-            o_data_w = (i_data_a[11] == 0) ? i_data_a : 12'd0;
-          end  // ReLU
+        3'b101: begin
+          o_data_w = (i_data_a[11] == 0) ? i_data_a : 12'd0;
+        end  // ReLU
 
-          3'b110: begin
-            mean_w   = i_data_a + i_data_b;
-            o_data_w = mean_w >> 1;
-          end  // MEAN
+        3'b110: begin
+          mean_w   = i_data_a + i_data_b;
+          o_data_w = mean_w >> 1;
+        end  // MEAN
 
-          3'b111: begin
-            abs_a_w  = (i_data_a[11] == 1'b1) ? (~i_data_a + 1) : i_data_a;
-            abs_b_w  = (i_data_b[11] == 1'b1) ? (~i_data_b + 1) : i_data_b;
-            o_data_w = (abs_a_w > abs_b_w) ? abs_a_w : abs_b_w;
-          end  // ABSOLUTE MAX
+        3'b111: begin
+          abs_a_w  = (i_data_a[11] == 1'b1) ? (~i_data_a + 1) : i_data_a;
+          abs_b_w  = (i_data_b[11] == 1'b1) ? (~i_data_b + 1) : i_data_b;
+          o_data_w = (abs_a_w > abs_b_w) ? abs_a_w : abs_b_w;
+        end  // ABSOLUTE MAX
 
-          default: begin
-            o_data_w = 12'b0;
-            o_valid_w = 1'b0;
-            o_overflow_w = 1'b0;
-          end  // Default case
+        default: begin
+          o_data_w = 12'b0;
+          o_valid_w = 1'b0;
+          o_overflow_w = 1'b0;
+        end  // Default case
 
-        endcase
+      endcase
     end
   end
 
@@ -129,7 +144,7 @@ module alu (
   // ---------------------------------------------------------------------------
   // ---- Write your sequential block design here ---- //
 
-  always @(negedge i_clk or negedge i_rst_n) begin
+  always @(posedge i_clk or negedge i_rst_n) begin
     if (!i_rst_n) begin
       o_data_r <= 0;
       o_overflow_r <= 0;
--- 1101_hw1/xcelium.d/run.lnx8664.24.03.d/files.ts
+++ 1101_hw1/xcelium.d/run.lnx8664.24.03.d/files.ts
@@ -1,2 +1,2 @@
-1770738683 /home/v72211/miniProject1/ALU-Design/1101_hw1/alu.v
+1770857686 /home/v72211/miniProject1/ALU-Design/1101_hw1/alu.v
 1770574219 /home/v72211/miniProject1/ALU-Design/1101_hw1/testbench.v
